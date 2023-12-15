0f1be14da325f820b75e81c83e6e896476473705
Nawin Waree <nwaree@arete.com>
2023-12-15 14:51:11 -0500

    Eureka

--- 12/d12_2.c
+++ 12/d12_2.c
@@ -8,33 +8,39 @@
 
 typedef struct
 {
-    int *data;
-    int *dims;
-    int n_dims;
+    int64_t *data;
+    int64_t *dims;
+    int64_t n_dims;
 } Mat;
 
-int* at(Mat *mat, int *indices)
+int64_t* at(Mat *mat, int64_t *indices)
 {
-    int *val = mat->data;
-    int stride = 1;
-    for (int i = 0; i < mat->n_dims; i++)
+    int64_t *val = mat->data;
+    int64_t stride = 1;
+    for (int64_t i = 0; i < mat->n_dims; i++)
     {
+        if (indices[i] < 0 || indices[i] >= mat->dims[i])
+        {
+            printf("ERROR\n");
+            exit(-1);
+        }
         val += indices[i] * stride;
         stride *= mat->dims[i];
     }
     return val; 
 }
 
-Mat* make_mat3(int rows, int cols, int channels, int default_val)
+Mat* make_mat3(int64_t rows, int64_t cols, int64_t channels, int64_t default_val)
 {
     Mat* ret = (Mat*)malloc(sizeof(Mat));
     ret->n_dims = 3;
-    ret->dims = (int*)malloc(sizeof(ret->dims[0]) * ret->n_dims);
+    ret->dims = (int64_t*)malloc(sizeof(ret->dims[0]) * ret->n_dims);
     ret->dims[0] = cols;
     ret->dims[1] = rows;
     ret->dims[2] = channels;
+    ret->data = (int64_t*)malloc(sizeof(ret->data[0]) * cols * rows * channels);
 
-    for (int i = 0; i < cols * rows * channels; i++)
+    for (int64_t i = 0; i < cols * rows * channels; i++)
         ret->data[i] = default_val;
 
     return ret;
@@ -47,23 +53,49 @@ void delete_mat(Mat* mat)
     free(mat);
 }
 
-int count_arrangements(char *str, int len, int *record, int record_len, int count, int idx, int record_idx, Mat* memo)
+int64_t count_arrangements(char *str, int64_t len, int64_t *record, int64_t record_len, int64_t count, int64_t idx, int64_t record_idx, Mat* memo)
 {
-    int memo_val = *at(memo, (int[]){count, idx, record_idx});
-    if (memo_val != -1)
-        return memo_val;
+    if (idx == len)
+    {
+        if (count == 0)
+            return record_idx == record_len;
+        else
+            return record_idx == record_len -1 && record[record_idx] == count;
+    }
 
-    
+    int64_t* memo_val = at(memo, (int64_t[]){count, idx, record_idx});
+    if (*memo_val != -1)
+        return *memo_val;
+
+    int64_t val = 0;
+    int64_t record_match = record_idx < record_len && record[record_idx] == count;
+    switch (str[idx])
+    {
+        case '?':
+            val += count_arrangements(str, len, record, record_len, count + 1, idx + 1, record_idx, memo);
+        case '.':
+            if (count == 0 || record_match)
+                val += count_arrangements(str, len, record, record_len, 0, idx + 1, record_idx + record_match, memo);
+            break;
+        case '#':
+            val += count_arrangements(str, len, record, record_len, count + 1, idx + 1, record_idx, memo);
+            break;
+        default:
+            printf("ERROR: %c\n", str[idx]);
+            exit(-1);
+    }
+    *memo_val = val;
+    return val;
 }
 
-int64_t main(int64_t argc, char* argv[])
+int main(int argc, char* argv[])
 {
     FILE *f = fopen(argv[1], "r");
     char buf[1024];
-    int record[1024];
-    int record_len;
+    int64_t record[1024];
+    int64_t record_len;
 
-    int total = 0;
+    int64_t total = 0;
     while (!feof(f))
     {
         record_len = 0;
@@ -78,29 +110,30 @@ int64_t main(int64_t argc, char* argv[])
             tok = strtok(NULL, ",");
         }
 
-#if 1
+#if 0
         *sep = '\0';
-        int len = strlen(buf);
+        int64_t len = strlen(buf);
 #else
         *sep = '?';
-        int len = sep - buf + 1;
+        int64_t len = sep - buf + 1;
 
-        for (int i = 0; i < 4; i++)
+        for (int64_t i = 0; i < 4; i++)
         {
-            memcpy(record + record_len * (i + 1), record, sizeof(int) * record_len);
+            memcpy(record + record_len * (i + 1), record, sizeof(int64_t) * record_len);
             memcpy(buf + len * (i + 1), buf, len);
         }
-        buf[len * 5 - 1] = '\0';
+        len = len * 5 - 1;
+        buf[len] = '\0';
         record_len *= 5;
 #endif
-        Mat* memo = make_mat3(len, len, record_len, -1);
+        Mat* memo = make_mat3(len, len, record_len + 1, -1);
 
-        int count = count_arrangements(buf, len, record, record_len, 0, 0, 0, memo);
+        int64_t count = count_arrangements(buf, len, record, record_len, 0, 0, 0, memo);
         total += count;
-        printf("%d\n", count);
+        printf("%ld\n", count);
 
         delete_mat(memo);
     }
-    printf("%d\n", total);
+    printf("%ld\n", total);
 }
 
